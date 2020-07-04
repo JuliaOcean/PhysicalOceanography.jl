9558c8e5654289d87b7bc71e29e26f27a1ef9bfc
gaelforget <gforget@mit.edu>
2020-07-04 12:53:24 -0400

    Coriolis & Rossby parameters

--- src/PhysicalOceanography.jl
+++ src/PhysicalOceanography.jl
@@ -1,5 +1,22 @@
 module PhysicalOceanography
 
-# Write your package code here.
+export f, β
 
+Ω = 7.2921 * 10^(-5) #rad/s
+R = 6.3781 * 10^(6) #m
+
+"""
+    f(φ)
+
+Coriolis Parameter
+"""
+f(φ) = 2Ω * sind(φ)
+
+"""
+    β(φ)
+
+Rossby Parameter
+""" 
+β(φ) = 2Ω / R * cosd(φ)
+ 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using PhysicalOceanography
 using Test
 
 @testset "PhysicalOceanography.jl" begin
-    # Write your tests here.
+
+    @test isapprox(f(45),1.03125867182e-4)
+    @test isapprox(β(45),1.61687441686e-11)
+
 end
