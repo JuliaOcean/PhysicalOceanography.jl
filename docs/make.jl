9dd332e1783786c56d178b5848a772e0fd79ad78
gaelforget <gforget@mit.edu>
2020-07-16 05:05:44 -0400

    add literate example in doc [1/n]

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,26 @@
 using PhysicalOceanography
-using Documenter
+using Documenter, Literate
+
+# generate tutorials and how-to guides using Literate
+src = joinpath(@__DIR__, "src")
+lit = joinpath(@__DIR__, "../examples")
+notebooks = joinpath(src, "notebooks")
+
+execute = true # Set to true for executing notebooks and documenter!
+nb = true      # Set to true to generate the notebooks
+for (root, _, files) in walkdir(lit), file in files
+    splitext(file)[2] == ".jl" || continue
+    ipath = joinpath(root, file)
+    opath = splitdir(replace(ipath, lit=>src))[1]
+    println(ipath)
+    println(opath)
+    Literate.markdown(ipath, opath, documenter = execute)
+    nb && Literate.notebook(ipath, notebooks, execute = execute)
+end
+
+# Documentation structure
+ismd(f) = splitext(f)[2] == ".md"
+pages(folder) = [joinpath(folder, f) for f in readdir(joinpath(src, folder)) if ismd(f)]
 
 makedocs(;
     modules=[PhysicalOceanography],
@@ -13,6 +34,8 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
+        "One Example" => "ex_f.md",
+        "More Examples" => pages("Parameters"),
     ],
 )
 
--- examples/ex_f.jl
+++ examples/ex_f.jl
@@ -1,6 +1,9 @@
 
 # # Coriolis Parameter
 #
+#md # [![](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/notebooks/example_f.ipynb)
+#md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/notebooks/example_f.ipynb)
+#
 # [from wikipedia:](https://en.wikipedia.org/wiki/Coriolis_frequency)
 #
 # The Coriolis frequency `ƒ`, also called the Coriolis parameter or Coriolis
@@ -8,8 +11,6 @@
 # by the `sine` of the latitude `φ`.
 #
 # $$ f=2\Omega \sin \varphi $$
-#
-# [![Binder](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/path/to/notebook.inpynb)
 
 using PhysicalOceanography
 isapprox(f(45),1.03125867182e-4)
